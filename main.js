b91a40b83c6dfabe1053354394bddfa117185f7e
Davis Patterson <DavisFPatterson@gmail.com>
2023-07-28 18:52:51 -0400

    added now playing info up top then broke now playing img

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const searchButton = document.querySelector('#searchButton')
 const form = document.querySelector('form')
 const apiUrl = "https://itunes.apple.com/search?entity=musicTrack&term="; 
 const musicPlayer = document.getElementById('musicPlayer')
+const currentSongContainer = document.getElementById('currentSongContainer');
+const currentSongImg = document.getElementById('currentSongImg');
+const currentSongName = document.getElementById('currentSongName');
+const currentSongArtist = document.getElementById('currentSongArtist');
 let input = document.querySelector('#search-input')
 
 function replaceSpaces(searchQuery) {
@@ -30,8 +34,18 @@ form.addEventListener('submit', (event) => {
             let songBox = document.createElement("div");
             songBox.classList.add("songBox");
             songBox.addEventListener('click', () => {
+                currentSongImg.style.display = 'none';
+                currentSongBox.style.display = 'none';
+                currentSongImg.src = '';
+                currentSongName.textContent = '';
+                currentSongArtist.textContent = '';
                 musicPlayer.src = song.previewUrl;
                 musicPlayer.play();
+                currentSongImg.src = song.artworkUrl60;
+                currentSongName.textContent = song.trackName;
+                currentSongArtist.textContent = song.artistName;
+                currentSongBox.style.display = 'block';
+                currentSongImg.style.display = 'block';
             })
             let image = document.createElement("img");
             image.classList.add("image");
