54a5f4eb94e1b96c9b3150a3cd7706a33f433db7
Davis Patterson <DavisFPatterson@gmail.com>
2023-07-30 02:26:20 -0400

    darkmode is backwards and broke the default color scheme

--- darkmode.js
+++ darkmode.js
@@ -1,6 +1,29 @@
 let darkMode = localStorage.getItem('darkMode');
-const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const darkModeToggle = document.querySelector('.dark-mode-toggle');
 
-darkModeToggle.addEventListener("click", () => {
-    console.log('test'); 
-})
+const enableDarkMode = () => {
+  document.body.classList.add('darkMode');
+  localStorage.setItem('darkMode', 'enabled');
+};
+
+const disableDarkMode = () => {
+  document.body.classList.remove('darkMode');
+  localStorage.setItem('darkMode', null);
+};
+
+if (darkMode === 'enabled') {
+  enableDarkMode();
+} else {
+  disableDarkMode();
+}
+
+darkModeToggle.addEventListener('click', () => {
+  darkMode = localStorage.getItem('darkMode');
+  if (darkMode !== 'enabled') {
+    enableDarkMode();
+    console.log(darkMode);
+  } else {
+    disableDarkMode();
+    console.log(darkMode);
+  }
+});
