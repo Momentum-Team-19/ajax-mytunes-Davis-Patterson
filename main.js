33d9282fa26847695e887add5e34cbbc7586dc79
Davis Patterson <DavisFPatterson@gmail.com>
2023-07-30 00:11:19 -0400

    stylizing things and added darkmode.js

--- main.js
+++ main.js
@@ -1,53 +1,75 @@
-const searchResults = document.querySelector('#searchResults')
-const searchButton = document.querySelector('#searchButton')
-const form = document.querySelector('form')
-const apiUrl = "https://itunes.apple.com/search?entity=musicTrack&term=";
+const searchResults = document.querySelector('#searchResults');
+const searchButton = document.querySelector('#searchButton');
+const form = document.querySelector('form');
+const apiUrl = 'https://itunes.apple.com/search?entity=musicTrack&term=';
 const dynamicContainer = document.getElementById('dynamicContainer');
-let input = document.querySelector('#search-input')
+let input = document.querySelector('#search-input');
 let musicPlayer;
-let musicPlayerContainer; 
+let musicPlayerContainer;
 
 function replaceSpaces(searchQuery) {
-    return searchQuery.replace(/ /g, '+');
+  return searchQuery.replace(/ /g, '+');
 }
 
 function createResults(results) {
-    for (let song of results) {
-        let songBox = document.createElement("div");
-        songBox.classList.add("songBox");
-        //songBox img
-        let image = document.createElement("img");
-        image.classList.add("image");
-        let figure = document.createElement("figure");
-        figure.classList.add("imageBox");
-        figure.appendChild(image);
-        image.src = song.artworkUrl100;
-        songBox.appendChild(image);
-        let title = document.createElement("div");
-        title.classList.add("title");
-        title.innerText = song.trackName;
-        songBox.appendChild(title);
-        let artistDiv = document.createElement("div");
-        artistDiv.classList.add('artist');
-        artistDiv.innerText = song.artistName;
-        searchResults.appendChild(songBox);
-        songBox.appendChild(artistDiv);
+  //if results = 0
+  if (results.length === 0) {
+    displayMessage('No results found :(');
+    return;
+  }
 
-        //when click on songBox
-        songBox.addEventListener('click', () => {
-            const musicPlayer = createMusicPlayer();
-            musicPlayer.src = song.previewUrl;
-            createNowPlaying(song);
-            musicPlayer.play();
-            })
-    }
+  //if results > 0
+  for (let song of results) {
+    //create songBox
+    let songBox = document.createElement('div');
+    songBox.classList.add('songBox');
+    //create img
+    let image = document.createElement('img');
+    image.classList.add('image');
+    //create figure
+    let figure = document.createElement('figure');
+    figure.classList.add('imageBox');
+    figure.appendChild(image);
+    //img source
+    image.src = song.artworkUrl100;
+    songBox.appendChild(image);
+    let title = document.createElement('div');
+    title.classList.add('title');
+    title.innerText = song.trackName;
+    songBox.appendChild(title);
+    let artistDiv = document.createElement('div');
+    artistDiv.classList.add('artist');
+    artistDiv.innerText = song.artistName;
+    searchResults.appendChild(songBox);
+    songBox.appendChild(artistDiv);
+
+    //when click on songBox
+    songBox.addEventListener('click', () => {
+      const musicPlayer = createMusicPlayer();
+      musicPlayer.src = song.previewUrl;
+      createNowPlaying(song);
+      musicPlayer.play();
+    });
+  }
 }
 
 function displayMessage(message) {
-    const messageBox = document.createElement('div');
-    messageBox.id = 'messageBox';
-    messageBox.textContent = message;
-    searchResults.appendChild(messageBox); 
+  clearMessage();
+  const messageBox = document.createElement('div');
+  messageBox.id = 'messageBox';
+  messageBox.textContent = message;
+  searchResults.appendChild(messageBox);
+
+  setTimeout(() => {
+    searchResults.removeChild(messageBox);
+  }, 2000);
+}
+
+function clearMessage() {
+  const messageBox = document.getElementById('messageBox');
+  if (messageBox) {
+    searchResults.removeChild(messageBox);
+  }
 }
 
 /* what the createNowPlaying HTML looks like:
@@ -72,67 +94,72 @@ function displayMessage(message) {
 */
 
 function createNowPlaying(song) {
-    const playerContainer = document.createElement('div');
-    playerContainer.id = 'playerContainer';
+  const playerContainer = document.createElement('div');
+  playerContainer.id = 'playerContainer';
 
-    const currentSongContainer = document.createElement('div');
-    currentSongContainer.id = 'currentSongContainer';
+  const currentSongContainer = document.createElement('div');
+  currentSongContainer.id = 'currentSongContainer';
 
-    //clear current song container
-    while (currentSongContainer.firstChild) {
-        currentSongContainer.removeChild(currentSongContainer.firstChild);
+  //clear current song container
+  while (currentSongContainer.firstChild) {
+    currentSongContainer.removeChild(currentSongContainer.firstChild);
+  }
+
+  if (musicPlayer.src !== song.previewUrl) {
+    musicPlayer.src = song.previewUrl;
+  }
+
+  if (musicPlayer.src) {
+    musicPlayer.src = song.previewUrl;
+    if (!musicPlayerContainer) {
+      musicPlayerContainer = document.createElement('div');
+      musicPlayerContainer.id = 'musicPlayerContainer';
     }
 
-    if (musicPlayer.src !== song.previewUrl) {
-        musicPlayer.src = song.previewUrl;
-    }
+    musicPlayerContainer.style.display = 'block';
+  } else {
+    musicPlayerContainer.style.display = 'none';
+  }
 
-    if (musicPlayer.src) {
-        musicPlayer.src = song.previewUrl;
-        if (!musicPlayerContainer) {
-            musicPlayerContainer = document.createElement('div');
-            musicPlayerContainer.id = 'musicPlayerContainer';
-        }
+  //build "now playing" box
+  let currentSongBox = document.createElement('div');
+  currentSongBox.id = 'currentSongBox';
 
-        musicPlayerContainer.style.display = 'block';
-    } else {
-        musicPlayerContainer.style.display = 'none';
-    }
+  //now playing img
+  let currentSongImg = document.createElement('img');
+  currentSongImg.id = 'currentSongImg';
+  currentSongImg.src = song.artworkUrl100;
 
-    //build "now playing" box
-    let currentSongBox = document.createElement("div");
-    currentSongBox.id = "currentSongBox";
+  //now playing info
+  let currentSongInfo = document.createElement('div');
+  currentSongInfo.id = 'currentSongInfo';
 
-    //now playing img
-    let currentSongImg = document.createElement("img");
-    currentSongImg.id = "currentSongImg";
-    currentSongImg.src = song.artworkUrl100;
+  //now playing name
+  let currentSongName = document.createElement('p');
+  currentSongName.id = 'currentSongName';
+  currentSongName.textContent = song.trackName;
 
-    //now playing info
-    let currentSongInfo = document.createElement("div");
-    currentSongInfo.id = "currentSongInfo";
+  //now playing artist
+  let currentSongArtist = document.createElement('p');
+  currentSongArtist.id = 'currentSongArtist';
+  currentSongArtist.textContent = song.artistName;
 
-    //now playing name
-    let currentSongName = document.createElement("p");
-    currentSongName.id = "currentSongName";
-    currentSongName.textContent = song.trackName;
+  currentSongBox.appendChild(currentSongImg);
+  currentSongBox.appendChild(currentSongInfo);
 
-    //now playing artist
-    let currentSongArtist = document.createElement("p");
-    currentSongArtist.id = "currentSongArtist";
-    currentSongArtist.textContent = song.artistName;
+  currentSongInfo.appendChild(currentSongName);
+  currentSongInfo.appendChild(currentSongArtist);
 
-    currentSongBox.appendChild(currentSongImg);
-    currentSongBox.appendChild(currentSongInfo);
-    
-    currentSongInfo.appendChild(currentSongName);
-    currentSongInfo.appendChild(currentSongArtist);
+  const existingPlayerContainer = document.getElementById('playerContainer');
+  if (existingPlayerContainer) {
+    dynamicContainer.removeChild(existingPlayerContainer);
+  }
 
-    playerContainer.appendChild(currentSongContainer);
-    playerContainer.appendChild(musicPlayerContainer);
-    dynamicContainer.appendChild(playerContainer);
+  playerContainer.appendChild(currentSongContainer);
+  playerContainer.appendChild(musicPlayerContainer);
+  dynamicContainer.appendChild(playerContainer);
 
-    currentSongContainer.appendChild(currentSongBox)
+  currentSongContainer.appendChild(currentSongBox);
 }
 
 /* what the createNowPlaying HTML looks like:
@@ -157,47 +184,46 @@ function createNowPlaying(song) {
 */
 
 function createMusicPlayer() {
-    if (!musicPlayer) {
-        musicPlayer = document.createElement('audio');
-        musicPlayer.controls = true;
-        musicPlayer.preload = 'auto';
-        if (!musicPlayerContainer) {
-            musicPlayerContainer = document.createElement('div');
-            musicPlayerContainer.id = 'musicPlayerContainer';
-        }
-        musicPlayerContainer.appendChild(musicPlayer);
+  if (!musicPlayer) {
+    musicPlayer = document.createElement('audio');
+    musicPlayer.controls = true;
+    musicPlayer.preload = 'auto';
+    if (!musicPlayerContainer) {
+      musicPlayerContainer = document.createElement('div');
+      musicPlayerContainer.id = 'musicPlayerContainer';
     }
-    return musicPlayer;
+    musicPlayerContainer.appendChild(musicPlayer);
+  }
+  return musicPlayer;
 }
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    let searchTerm = input.value.trim(); 
-    if (searchTerm === "") {
-        displayMessage('Please enter a search term.')
-        return;
-    }
-    let apiUrlWithSearch = apiUrl + replaceSpaces(searchTerm) + "/";
-    console.log(apiUrlWithSearch);
-    //clear the previous search before new search
-    let searchResults = document.getElementById("searchResults")
-    searchResults.innerHTML = '';
-    fetch(apiUrlWithSearch, {
-        method: 'GET',
-        headers: {'Content-Type': 'application/json' },
-    })
+  event.preventDefault();
+  let searchTerm = input.value.trim();
+  if (searchTerm === '') {
+    displayMessage('Please enter a search term.');
+    return;
+  }
+  let apiUrlWithSearch = apiUrl + replaceSpaces(searchTerm) + '/';
+  console.log(apiUrlWithSearch);
+  //clear the previous search before new search
+  let searchResults = document.getElementById('searchResults');
+  searchResults.innerHTML = '';
+  fetch(apiUrlWithSearch, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+  })
     .then((response) => {
-        return response.json();
+      return response.json();
     })
     .then((data) => {
-        console.log(data.results)
-        while (searchResults.firstChild) {
-            searchResults.removeChild(searchResults.firsChild);
-        }
-        createResults(data.results);
+      console.log(data.results);
+      while (searchResults.firstChild) {
+        searchResults.removeChild(searchResults.firsChild);
+      }
+      createResults(data.results);
     })
     .catch((error) => {
-        displayMessage('An error occured while fetching the results.')
+      displayMessage('An error occured while fetching the results.');
     });
 });
-
